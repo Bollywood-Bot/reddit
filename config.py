031d33d650c07640e0d4f85198005fa503c4fce2
JATIN <jatin.dhar27@gmail.com>
2021-06-14 22:26:03 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-username = "RedditUsername"
-password = "password"
-client_id = "idGoesHere"
-client_secret = "secretGoesHere"
+username = "Bollywood-Bot"
+password = "Akshay-kumar"
+client_id = "5Je1DzC6vUoulw"
+client_secret = "FKqNWlkjg1yvxPovmDnoK4qwumBjoQ"
